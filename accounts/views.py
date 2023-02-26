ddeb9be9b29579a03ccae6321a77f4b9f7199cfc
mutemip <paulmutemi2030@gmail.com>
2023-02-27 01:47:26 +0300

    add: fun based register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
@@ -11,3 +14,16 @@ class SignUpView(generic.CreateView):
     template_name = "registration/signup.html"
 
 # using function based views
+
+def registerUser(request):
+    # form = NewUserForm()
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration Successful.")
+            return redirect("/")
+        messages.error(request, "Registration Unsuccessful.")
+    form = NewUserForm()
+    return render(request, "registration/register.html", {"form": form})
