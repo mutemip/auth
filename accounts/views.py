730b12ce6244e60709616854a749ea48f02e9f66
mutemip <paulmutemi2030@gmail.com>
2023-02-27 00:47:38 +0300

    view comments

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
 from django.urls import reverse_lazy
 
-# Create your views here.
+# Using Class based Views
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+# using function based views
