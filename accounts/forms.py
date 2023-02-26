353be1399a540f81a16fc664e27ca34b1198f5ee
mutemip <paulmutemi2030@gmail.com>
2023-02-27 02:00:54 +0300

    add: more form fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,17 @@ from django.contrib.auth.models import User
 
 # signup form
 class NewUserForm(UserCreationForm):
+    STAFF_STATUS = (
+        ("True", "True"),
+        ("False", "False")
+    )
+    first_name = forms.CharField(max_length=255)
+    last_name = forms.CharField(max_length=255)
     email = forms.EmailField(required=True)
+    is_staff = forms.BooleanField()
+
+    
+
 
     class Meta:
         model = User
@@ -13,7 +23,11 @@ class NewUserForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
+        user.is_staff = self.cleaned_data['is_staff']
+
         if commit:
             user.save()
         return user
